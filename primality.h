f188b9fd33412e663787b9d1c86052ce49e49ee5
Andrew Shallue <ashallue@iwu.edu>
2020-06-08 20:33:07 -0500

    created function stub in primality.h for pocklington primality test

--- primality.h
+++ primality.h
@@ -6,6 +6,8 @@ part of summer project constructing order 2 Carmichael numbers
 // Input is an integer n, output is a boolean, True for prime
 // Importantly, these are all proofs of primality
 
+#include <vector>
+
 using namespace std;
 
 // primality using trial division
@@ -14,6 +16,14 @@ bool is_prime_trialdivision(long n);
 // primality test via a base-b Fermat test.  False means 
 // provably composite.  True means probably prime.
 bool is_prime_fermat(long n, long b);
+
+/* First version of Pocklington test.  This algorithm attempts to prove a number is prime.
+If the number is prime, the running time is expected polynomial.  If the number is composite, 
+it may be very slow, so make sure to apply a primality test first.
+Input is n along with a factorization given by a pair of vector<longs>
+Output is a bool.  True means proven prime, False means failed to prove prime.
+*/
+bool pocklington1(long n, vector<long> prime_factors, vector<long> powers);
 
 // Helper functions
 // binary modular powering, computes a^e mod n
