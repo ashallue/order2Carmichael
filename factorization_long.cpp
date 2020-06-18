3a2df21271aeb2bd5a69bbec4806f85d5d46cefe
Andrew Shallue <ashallue@iwu.edu>
2020-06-17 19:57:10 -0500

    deleted main function from factorization, renamed Factorization as FactorizationLong, because I want to make a ZZ version

---
+++ factorization_long.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class FactorizationLong{
+  private:
+    //factors of a number
+    //a vector of all the prime factors
+    //a vector of powers for each factor
+    vector<long> factors;
+    vector<long> primes; 
+    vector<long> powers;
+  public:
+    //constructor to initialize factors
+    Factorization(vector<long> f){
+      factors = f;
+    }
+
+    //returns all the prime factors of x
+    vector<long> getPrimeFactors(){ 
+      return primes;
+    }
+
+    //returns powers of factors
+    vector<long> getPowers(){
+      return powers;
+    }
+
+    //returns true when the length of vector<long> primes
+    //and vector<long> powers are the same
+    bool isSameLength(){
+      if(primes.size() == powers.size()){
+        return true;
+      }
+      return false;
+    }
+
+    //the product of all the factors
+    long factorProduct(){
+      long product = 1;
+      for(long i = 0; i < factors.size(); ++i){
+        product *= factors.at(i);
+      }
+      return product;
+    }
+};
+
+
